d948c34904b17ab620930c48d4e718b67a5b11ca
Ari Silburt <silburt@astro.utoronto.ca>
2018-06-16 21:09:20 -0400

    features

---
+++ gitfeatures.py
@@ -0,0 +1,77 @@
+import requests
+from requests.auth import HTTPBasicAuth
+from datetime import datetime
+import json
+
+# base directory is from ./run.py
+auth = open('utils/auth.txt').read()
+username, pw = auth.split()[0], auth.split()[1]
+
+def scrape_readme_single(dir, r):
+    git_path = r.split('repos/')[1]
+    url = 'https://raw.githubusercontent.com/%s/master/README'%git_path
+    try:
+        readme = requests.get('%s.md'%url,
+                              headers={"Accept":"application/vnd.github.mercy-preview+json"},
+                              auth=HTTPBasicAuth(username, pw))
+        if(readme.ok):
+            readme = readme.text
+        else:
+            readme = requests.get('%s.rst'%url,
+                                  headers={"Accept":"application/vnd.github.mercy-preview+json"},
+                                  auth=HTTPBasicAuth(username, pw)).text
+          
+            # write to file
+            readme_corpus = open('%s/%s.txt'%(dir, git_path.split('/')[1]), 'w')
+            readme_corpus.write(str(readme.encode('utf8')))
+            readme_corpus.close()
+    except:
+        print('No README file for %s. Skipping'%r)
+
+# get frequency of desired packages in python code
+def get_package_freq(text, GProfile):
+    for p in GProfile.packages.keys():
+        if text.find(p) != -1:    # is package even in script at all?
+            
+            # direct import, e.g. 'import numpy (as np)'
+            string = 'import %s'%p
+            loc = text.find(string)
+            if loc != -1:
+                # last word in line is call_name
+                call_name = text[loc:].partition('\n')[0].split()[-1].split('\n')[0].split('\\')[0]
+                GProfile.packages[p] += text.count('%s.'%call_name)
+            
+            # indirect import, e.g. 'from numpy.X import vectorize (as vec)'
+            string_from = 'from %s'%p
+            loc_from = text.find(string_from)
+            ext = ['(', '.']  # check both X() and X.y()
+            while loc_from != -1: # iterate same package imported mult. times on different lines
+                try:
+                    call_names = text[loc_from:].partition('\n')[0].split(' import ')[1]
+                    if call_names.count(',') != 0:  # iterate mulitple imports on same line
+                        call_names = call_names.replace('(','').replace(')','').replace(" ", "").split(',')
+                        for call_name in call_names:
+                            if call_name == "": # skip bad entries
+                                continue
+                            GProfile.packages[p] += sum(text.count('%s%s'%(call_name, x)) for x in ext)
+                    else:
+                        call_name = call_names.split()[-1]
+                        GProfile.packages[p] += sum(text.count('%s%s'%(call_name, x)) for x in ext)
+                except:
+                    pass
+                loc_from = text.find(string_from, loc_from + 1) # get next instance, e.g. 'from numpy.X import b'
+
+def get_repo_commit_history(commits_url, GProfile):
+    try:
+        r = requests.get(commits_url, headers={"Accept":"application/vnd.github.mercy-preview+json"},
+                     auth=HTTPBasicAuth(username, pw))
+        commits = json.loads(r.text or r.content)
+        for commit in commits:
+            date_string = commit['commit']['author']['date']
+            date = datetime.strptime(date_string.split("T")[0],'%Y-%m-%d')
+            GProfile.commit_history.append(date)
+    except:
+        print('couldnt get commit history for %s'%commits_url)
+
+
+
