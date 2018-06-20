ae7423a3bca30a30e793b1a5789b81b56fa30ce2
Ari Silburt <silburt@astro.utoronto.ca>
2018-06-19 21:36:41 -0400

    bug fixes

--- gitfeatures.py
+++ gitfeatures.py
@@ -21,83 +21,45 @@ def get_request(url, timeout=10):
         i += 1
     return r
 
-#def scrape_readme_single(dir, r):
-#    git_path = r.split('repos/')[1]
-#    url = 'https://raw.githubusercontent.com/%s/master/README'%git_path
-#    try:
-#        readme = requests.get('%s.md'%url,
-#                              headers={"Accept":"application/vnd.github.mercy-preview+json"},
-#                              auth=HTTPBasicAuth(username, pw))
-#        if(readme.ok):
-#            readme = readme.text
-#        else:
-#            readme = requests.get('%s.rst'%url,
-#                                  headers={"Accept":"application/vnd.github.mercy-preview+json"},
-#                                  auth=HTTPBasicAuth(username, pw)).text
-#
-#            # write to file
-#            readme_corpus = open('%s/%s.txt'%(dir, git_path.split('/')[1]), 'w')
-#            readme_corpus.write(str(readme.encode('utf8')))
-#            readme_corpus.close()
-#    except:
-#        print('No README file for %s. Skipping'%r)
-
-def get_readme_length(path, GProfile):
-    git_path = path.split('repos/')[1]
-    url = 'https://raw.githubusercontent.com/%s/master/README'%git_path
-    try:
-        readme = get_request('%s.md'%url)
-        if(readme.ok):
-            readme = readme.text
-        else:
-            readme = get_request('%s.md'%url).text
-        
-        GProfile.readme_lines = len(readme.split('\n'))
-    except:
-        print('No README file for %s. Skipping'%r)
-
-# get frequency of desired packages in python code
-def get_package_freq(text, GProfile):
-    for p in GProfile.packages.keys():
-        if text.find(p) != -1:    # is package even in script at all?
-            
-            # direct import, e.g. 'import numpy (as np)'
-            string = 'import %s'%p
-            loc = text.find(string)
-            if loc != -1:
-                # last word in line is call_name
-                call_name = text[loc:].partition('\n')[0].split()[-1].split('\n')[0].split('\\')[0]
-                GProfile.packages[p] += text.count('%s.'%call_name)
-            
-            # indirect import, e.g. 'from numpy.X import vectorize (as vec)'
-            string_from = 'from %s'%p
-            loc_from = text.find(string_from)
-            ext = ['(', '.']  # check both X() and X.y()
-            while loc_from != -1: # iterate same package imported mult. times on different lines
-                try:
-                    call_names = text[loc_from:].partition('\n')[0].split(' import ')[1]
-                    if call_names.count(',') != 0:  # iterate mulitple imports on same line
-                        call_names = call_names.replace('(','').replace(')','').replace(" ", "").split(',')
-                        for call_name in call_names:
-                            if call_name == "": # skip bad entries
-                                continue
-                            GProfile.packages[p] += sum(text.count('%s%s'%(call_name, x)) for x in ext)
-                    else:
-                        call_name = call_names.split()[-1]
-                        GProfile.packages[p] += sum(text.count('%s%s'%(call_name, x)) for x in ext)
-                except:
-                    pass
-                loc_from = text.find(string_from, loc_from + 1) # get next instance, e.g. 'from numpy.X import b'
+def get_readme_length(contents_url, GProfile):
+    r = get_request(contents_url)
+    if r.ok:
+        contents = json.loads(r.text or r.content)
+        readme_url = None
+        for c in contents:
+            if 'README' in c['name']:
+                readme_url = c['download_url']
+                break
+        if readme_url:
+            readme = get_request(readme_url)
+            if(readme.ok):
+                text = readme.text
+                while "\n\n" in text:
+                    text = text.replace("\n\n","\n")
+                GProfile.readme_lines = len(text.splitlines())
 
 # get frequency of comments vs. code
 def get_comment_code_ratio(text, GProfile):
-    for sym_s, sym_e in [('#','\n'),('"""', '"""')]:  #
+    for sym_s, sym_e in [('#','\n')]:
         start = text.find(sym_s)
         end = text.find(sym_e, start + 1)
+        comm_len = 0
         while start != -1:
-            GProfile.comment_lines += len(text[start: end].split('\n'))
+            comm_len += len(text[start: end].split('\n'))
             start = text.find(sym_s, end + 1)
             end = text.find(sym_e, start + 1)
+
+    for sym_s, sym_e in [('"""', '"""')]:
+        start = text.find(sym_s)
+        end = text.find(sym_e, start + 1)
+        doc_len = 0
+        while start != -1:
+            doc_len += len(text[start: end].split('\n'))
+            start = text.find(sym_s, end + 1)
+            end = text.find(sym_e, start + 1)
+
+    GProfile.comment_lines += comm_len
+    GProfile.docstring_lines += doc_len
 
 # get summary stats of pep8 errors
 def get_pep8_errs(text, GProfile, show_source = True):
@@ -113,19 +75,25 @@ def get_pep8_errs(text, GProfile, show_source = True):
         GProfile.pep8[label] += int(val)
     
     # cleanup
-    #os.remove('temp.py')
-    #os.remove('temp.txt')
+    os.remove('temp.py')
+    os.remove('temp.txt')
 
 # get distribution of commits over time
-def get_repo_commit_history(commits_url, GProfile):
+def get_repo_commit_history(item, GProfile):
     try:
-        r = get_request(commits_url.split('{/sha}')[0])
+        commits_url = item['commits_url'].split('{/sha}')[0]
+        r = get_request(commits_url)
         commits = json.loads(r.text or r.content)
         GProfile.n_commits = len(commits)
         for commit in commits:
             date_string = commit['commit']['author']['date']
             date = datetime.strptime(date_string.split("T")[0],'%Y-%m-%d')
             GProfile.commit_history.append(date)
+
+        # get commits/time
+        created = datetime.strptime(item['created_at'].split("T")[0],'%Y-%m-%d')
+        updatelast = datetime.strptime(item['updated_at'].split("T")[0],'%Y-%m-%d')
+        GProfile.commits_per_time = (updatelast - created).days/float(GProfile.n_commits)
     except:
         print('couldnt get commit history for %s'%commits_url)
 
