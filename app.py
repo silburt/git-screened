ec28e122a7f10b267b8ede60c68f217b97c4df50
Ari Silburt <silburt@astro.utoronto.ca>
2018-06-21 12:54:00 -0400

    website starting to work

--- app.py
+++ app.py
@@ -1,166 +1,55 @@
 # https://stackoverflow.com/questions/45736656/how-to-use-a-button-to-trigger-callback-updates
+# https://dash.plot.ly/dash-core-components/upload
 
-import requests
-from requests.auth import HTTPBasicAuth
-import json
 import dash
 from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
-import signal
+import dash_table_experiments as dt
+import gitscraper as gs
+import gitfeatures as gf
+import json
 
-##################
-# base directory is from ./run.py
-auth = open('utils/auth.txt').read()
-username, pw = auth.split()[0], auth.split()[1]
-authtoken = open('utils/token.txt').read()
+app = dash.Dash()
 
-##################
-class TimeoutException(Exception):   # Custom exception class
-    pass
-
-def timeout_handler(signum, frame):   # Custom signal handler
-    raise TimeoutException
-
-##################
-def get_request(url, timeout=10):
-    r = None
-    i = 0
-    while r == None and i < 3:
-        try:
-            r = requests.get(url, headers={"Accept":"application/vnd.github.mercy-preview+json",
-                             "Authorization": "token %s"%authtoken},
-                             auth=HTTPBasicAuth(username, pw), timeout=timeout)
-        except:
-            print('tried request %d, no success'%i)
-        i += 1
-    return r
-
-def get_readme_length(contents_url, GProfile):
-    r = get_request(contents_url)
-    if r.ok:
-        contents = json.loads(r.text or r.content)
-        readme_url = None
-        for c in contents:
-            if 'README' in c['name']:
-                readme_url = c['download_url']
-                break
-        if readme_url:
-            readme = get_request(readme_url)
-            if(readme.ok):
-                text = readme.text
-                while "\n\n" in text:
-                    text = text.replace("\n\n","\n")
-                GProfile.readme_lines = len(text.splitlines())
-
-# get frequency of comments vs. code
-def get_comment_code_ratio(text, GProfile):
-    for sym_s, sym_e in [('#','\n')]:
-        start = text.find(sym_s)
-        end = text.find(sym_e, start + 1)
-        comm_len = 0
-        while start != -1:
-            comm_len += len(text[start: end].split('\n'))
-            start = text.find(sym_s, end + 1)
-            end = text.find(sym_e, start + 1)
-
-    for sym_s, sym_e in [('"""', '"""')]:
-        start = text.find(sym_s)
-        end = text.find(sym_e, start + 1)
-        doc_len = 0
-        while start != -1:
-            doc_len += len(text[start: end].split('\n'))
-            start = text.find(sym_s, end + 1)
-            end = text.find(sym_e, start + 1)
-
-    GProfile.comment_lines += comm_len
-    GProfile.docstring_lines += doc_len
-
-# get summary stats of pep8 errors
-def get_pep8_errs(text, GProfile, show_source = True):
-    ext = '10'
-    f = open('temp%s.py'%ext, 'w')
-    f.write(text)
-    f.close()
-    
-    call("pycodestyle --statistics -qq temp%s.py > temp%s.txt"%(ext, ext), shell=True)
-    errs = open('temp%s.txt'%ext, 'r').read().splitlines()
-    for err in errs:
-        val, label = err.split()[0], err.split()[1]
-        label = label[0:2] # remove extra details
-        GProfile.pep8[label] += int(val)
-    
-    # cleanup
-    os.remove('temp%s.py'%ext)
-    os.remove('temp%s.txt'%ext)
-
-# get distribution of commits over time
-def get_repo_commit_history(item, GProfile):
-    try:
-        commits_url = item['commits_url'].split('{/sha}')[0]
-        r = get_request(commits_url)
-        commits = json.loads(r.text or r.content)
-        GProfile.n_commits = len(commits)
-        for commit in commits:
-            date_string = commit['commit']['author']['date']
-            date = datetime.strptime(date_string.split("T")[0],'%Y-%m-%d')
-            GProfile.commit_history.append(date)
-        
-        # get commits/time
-        created = datetime.strptime(item['created_at'].split("T")[0],'%Y-%m-%d')
-        updatelast = datetime.strptime(item['updated_at'].split("T")[0],'%Y-%m-%d')
-        GProfile.commits_per_time = (updatelast - created).days/float(GProfile.n_commits)
-    except:
-        print('couldnt get commit history for %s'%commits_url)
-
-##################
-class Github_Profile:
-    def __init__(self):
-        self.user = ''
-        self.url = ''
-        
-        # metrics
-        self.commit_history = []
-        self.commits_per_time = 0
-        self.n_commits = 0
-        self.stargazers = 0
-        self.forks = 0
-        self.test_lines = 0
-        self.docstring_lines = 0
-        self.comment_lines = 0
-        self.readme_lines = 0
-        self.code_lines = 0
-        self.n_pyfiles = 0
-        self.pep8 = {}
-        a = ['E1','E2','E3','E4','E5','E7','E9','W1','W2','W3','W5','W6']
-        for p in a:
-            self.pep8[p] = 0
-
-##################
-def get_metrics_per_file(item, GProfile, test_file):
-    r = get_request(item['download_url'])
-    if r.ok:
-        text = r.text
-        
-        # metrics
-        get_comment_code_ratio(text, GProfile)
-        get_pep8_errs(text, GProfile)
-        
-        code_len = len(text.split('\n'))
-        GProfile.code_lines += code_len
-        if test_file:
-            GProfile.test_lines += code_len
-
-##################
-# recursive
+app.layout = html.Div([
+                       html.Label('Repository Name (format: user/repo)'),
+                       dcc.Input(id='repo', value='', type="text"),
+                       html.Button('Search', id='button'),
+                       html.Div(id='my-div', children='Enter a value and press Search'),
+                       
+#                       dcc.Upload(
+#                                  id='upload-data',
+#                                  children=html.Div([
+#                                                     'Drag and Drop or ',
+#                                                     html.A('Select Files')
+#                                                     ]),
+#                                  style={
+#                                  'width': '100%',
+#                                  'height': '60px',
+#                                  'lineHeight': '60px',
+#                                  'borderWidth': '1px',
+#                                  'borderStyle': 'dashed',
+#                                  'borderRadius': '5px',
+#                                  'textAlign': 'center',
+#                                  'margin': '10px'
+#                                  },
+#                                  # Allow multiple files to be uploaded
+#                                  multiple=True
+#                                  ),
+#                       html.Div(id='output-data-upload'),
+#                       html.Div(dt.DataTable(rows=[{}]), style={'display': 'none'})
+                       
+                       ])
+# So, it looks like dash can only reference one node away.
+# I.e. you can't reference an outside function that then references a different
+# Outside function
 def digest_repo(repo_url, GProfile):
-    r = get_request('%s'%repo_url)
+    r = gf.get_request('%s'%repo_url)
     if r.ok:
         repoItems = json.loads(r.text or r.content)
-        
-        signal.signal(signal.SIGALRM, timeout_handler)
+
         for item in repoItems:
-            signal.alarm(10)
             try:
                 test_file = 0
                 if item['type'] == 'file' and item['name'][-3:] == '.py':
@@ -168,21 +57,21 @@ def digest_repo(repo_url, GProfile):
                     if 'test' in item['name'].lower(): # pytest
                         test_file = 1
                     print(item['download_url'])
-                    get_metrics_per_file(item, GProfile, test_file)
+                    gs.get_metrics_per_file(item, GProfile, test_file)
                 elif item['type'] == 'dir':
                     digest_repo(item['url'], GProfile)
-            except TimeoutException:
+            except:
                 print('%s timed out, skipping!'%item['download_url'])
 
-##################
-def get_features(item, GP):
+def get_features(item):
+    GP = gs.Github_Profile([])
     contents_url = '%s/contents'%item['url']
     
     # scrape commit history
-    #get_repo_commit_history(item, GP)
+    gf.get_repo_commit_history(item, GP)
     
     # scrape readme
-    get_readme_length(contents_url, GP)
+    gf.get_readme_length(contents_url, GP)
     
     # scrape file-by-file stats
     digest_repo(contents_url, GP)
@@ -192,35 +81,19 @@ def get_features(item, GP):
     
     # scrape forks
     GP.forks = item['forks_count']
-    
     return GP
-
-
-##################
-##################
-##################
-app = dash.Dash()
-
-app.layout = html.Div([
-                       html.Label('Repository Name (format: user/repo)'),
-                       dcc.Input(id='repo', value='', type="text"),
-                       html.Button('Search', id='button'),
-                       html.Div(id='my-div')
-                       ])
 
 @app.callback(
               Output(component_id='my-div', component_property='children'),
               [Input('button', 'n_clicks')],
-              state=[State(component_id='repo', component_property='value')]
-)
+              state=[State(component_id='repo', component_property='value')])
 def update_output_div(n_clicks, input_value):
-    GP = Github_Profile()
-    r = get_request('https://api.github.com/repos/%s'%input_value)
+    r = gf.get_request('https://api.github.com/repos/%s'%input_value)
     if r.ok:
         item = json.loads(r.text or r.content)
-        GP = get_features(item, GP)
-        
-        return 'Stargazers for "{}" is {}'.format(input_value, GP.stargazers)
+        GP = get_features(item)
+        string = 'Pep8 errors for "{}" is {}, {}'.format(input_value, GP.pep8, GP.commits_per_time)
+        return string
     else:
         return 'Couldnt get stats for "{}"'.format(input_value)
 
