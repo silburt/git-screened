d35a83357e4fc046a61dd8d009f3b050f980e305
Ari Silburt <silburt@astro.utoronto.ca>
2018-06-28 14:17:17 -0400

    local gitscraper improvements

--- local_gitscraper.py
+++ local_gitscraper.py
@@ -14,7 +14,7 @@ def timeout_handler(signum, frame):   # Custom signal handler
 
 # Class for each Github Profile
 class Github_Profile:
-    def __init__(self, pckgs):
+    def __init__(self):
         self.user = ''
         self.url = ''
         
@@ -34,46 +34,41 @@ class Github_Profile:
         a = ['E1','E2','E3','E4','E5','E7','E9','W1','W2','W3','W5','W6']
         for p in a:
             self.pep8[p] = 0
-        
-        self.packages = {}
-        for p in pckgs:
-            self.packages[p] = 0
-
-def get_metrics_per_file(text, GProfile, test_file):
-        # metrics
-        #gf.get_package_freq(text, GProfile)
-        gf.get_comment_code_ratio(text, GProfile)
-        
-        code_len = len(text.split('\n'))
-        GProfile.code_lines += code_len
-        if test_file:
-            GProfile.test_lines += code_len
 
 # not recursive
 def digest_repo(file, GProfile):
     signal.signal(signal.SIGALRM, timeout_handler)
     signal.alarm(10)
-    test_file = 0
     try:
         GProfile.n_pyfiles += 1
         
         text = open(file, 'r').read()
-        if 'test' in file.lower().split('/')[-1]: # pytest
-            test_file = 1
-        get_metrics_per_file(text, GProfile, test_file)
+        
+        # code lines
+        code_len = len(text.splitlines())
+        GProfile.code_lines += code_len
+        
+        # test lines
+        if file.split('/')[-1].lower()[:5] == 'test_' and 'assert' in text: # pytest
+            GProfile.test_lines += code_len
+        
+        # comments
+        gf.get_comment_code_ratio(text, GProfile)
+        
+        # pep8 errors
         gf.get_pep8_errs(file, GProfile)
 
     except TimeoutException:
         print('%s timed out, skipping!'%file)
 
-def get_features(repo_list_dir, output_dir):
-    #repos = glob.glob('%s/*'%repo_list_dir)
-    repos = ['misc_scripts/']#, 'misc_plots/']
+def get_features(repo_dir, output_dir):
+    repos = glob.glob('%s/*'%repo_dir)
+    
     # Change the behavior of SIGALRM
     signal.signal(signal.SIGALRM, timeout_handler)
     for repo in repos:
         print(repo)
-        GP = Github_Profile([])
+        GP = Github_Profile()
         # scrape readme
         gf.get_readme_length(repo, GP)
         
@@ -97,8 +92,7 @@ def get_features(repo_list_dir, output_dir):
         string = '%s, %d, %d, %d, %d, %d, %d'
         data = open(output_dir, 'a')
         data.write(string%(repo, GP.n_pyfiles, GP.code_lines, GP.comment_lines,
-                           GP.docstring_lines, GP.test_lines,
-                           GP.readme_lines))
+                           GP.docstring_lines, GP.test_lines, GP.readme_lines))
 
         for key in GP.pep8.keys():
             data.write(', %d'%GP.pep8[key])
@@ -107,8 +101,7 @@ def get_features(repo_list_dir, output_dir):
 
 if __name__ == '__main__':
     repotype = 'bottom'
-    repo_dir = '%s_repos/'%repotype
-    #output_dir = "repo_data/%s_stars_stats_Python_local.txt"%repotype
-    output_dir = 'misc_scripts_testttttt.txt'
+    repo_dir = 'clone_repo_%s/'%repotype
+    output_dir = 'repo_data/%s_stars_stats_Python_FULL.txt'%repotype
     get_features(repo_dir, output_dir)
 
